891096fd9d8b4c73483aa66fcdfcf12844d23d22
harshsprinklr <harsh.saini@sprinklr.com>
2024-06-06 16:37:44 +0530

    changed build.gradle now sonarqube working and added issues in HelloModule1

--- module1/src/main/java/com/example/module1/HelloModule1.java
+++ module1/src/main/java/com/example/module1/HelloModule1.java
@@ -8,16 +8,21 @@ public class HelloModule1 {
         return "Hello from Module 1 and " + module2.sayHello();
     }
 
-    public void method1() {
-        int s = 2;
-        int y = 3;
-        int x = s + y;
-        System.out.println("Hello, world!" + x);
+    public String sayHello(int f) {
+        HelloModule2 module2 = new HelloModule2();
+        return "Hello from Module 1 and " + module2.sayHello();
     }
 
-    public void method2() {
-        System.out.println("Hello, world!");  // SonarLint will flag this as duplicate code
-    }
+//    public void method1() {
+//        int s = 2;
+//        int y = 3;
+//        int x = s + y;
+//        System.out.println("Hello, world!" + x);
+//    }
+//
+//    public void method2() {
+//        System.out.println("Hello, world!");  // SonarLint will flag this as duplicate code
+//    }
 
     public static void main(String[] args){
         HelloModule1 hello = new HelloModule1();
@@ -25,6 +30,9 @@ public class HelloModule1 {
 
         //TODO
 
+
         //TODO
+
+
     }
 }
