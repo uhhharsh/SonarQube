4fadeabb1a159b2cce38abcc88c93e70ed2e2ba7
harshsprinklr <harshsainix1@gmail.com>
2024-07-05 13:37:41 +0530

    PR test

--- module1/src/main/java/com/example/module1/HelloModule1.java
+++ module1/src/main/java/com/example/module1/HelloModule1.java
@@ -4,6 +4,9 @@ import com.example.module2.HelloModule2;
 
 public class HelloModule1 {
     public String sayHello() {
+
+        System.out.println("Testing PR");
+
         HelloModule2 module2 = new HelloModule2();
         return "Hello from Module 1 and " + module2.getMessage();
     }
