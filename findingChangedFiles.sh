632b4d46237371254c9a707e2affe141751ae9f0
harshsprinklr <harshsainix1@gmail.com>
2024-07-08 16:41:35 +0530

    updating findingChangedFiles.sh

--- findingChangedFiles.sh
+++ findingChangedFiles.sh
@@ -1,3 +1,13 @@
-{
-  git diff --name-only test main
-} | sort -u > /Users/harsh.saini/Desktop/multi-module-project/parent/changedFiles.txt
+#!/bin/bash
+
+# Check if running in Jenkins environment
+if [ "$JENKINS_ENV" == "true" ]; then
+    # Set workspace path dynamically
+    workspace="$WORKSPACE"
+else
+    # Local environment setup (replace with your local path)
+    workspace="/Users/harsh.saini/Desktop/multi-module-project/parent"
+fi
+
+# Run git diff command and save output to changedFiles.txt
+git diff --name-only test main | sort -u > "$workspace/changedFiles.txt"
