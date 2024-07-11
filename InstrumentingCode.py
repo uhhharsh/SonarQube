36893e0989195a2b5c0795b2d840403be197858d
harshsprinklr <harshsainix1@gmail.com>
2024-07-11 20:14:36 +0530

    Removing hardcoded stuff

--- InstrumentingCode.py
+++ InstrumentingCode.py
@@ -1,17 +1,27 @@
+import os
 import subprocess
 import re
 
 def run_git_diff():
+    source_branch = os.getenv('CI_MERGE_REQUEST_SOURCE_BRANCH_NAME')
+    target_branch = os.getenv('CI_MERGE_REQUEST_TARGET_BRANCH_NAME')
+    if not source_branch or not target_branch:
+        raise ValueError("Environment variables CI_MERGE_REQUEST_SOURCE_BRANCH_NAME and CI_MERGE_REQUEST_TARGET_BRANCH_NAME must be set")
+
     print("Running git diff command...")
-    result = subprocess.run(['git', 'diff', '-U0', 'remotes/origin/main', 'remotes/origin/test', '--', '*.java'], capture_output=True, text=True)
+    result = subprocess.run(['git', 'diff', '-U0', f'remotes/origin/{target_branch}', f'remotes/origin/{source_branch}', '--', '*.java'], capture_output=True, text=True)
     print("Git diff command output:\n", result.stdout)
     return result.stdout
 
 def run_git_diff_for_changed_files_names():
+    source_branch = os.getenv('CI_MERGE_REQUEST_SOURCE_BRANCH_NAME')
+    target_branch = os.getenv('CI_MERGE_REQUEST_TARGET_BRANCH_NAME')
+    if not source_branch or not target_branch:
+        raise ValueError("Environment variables CI_MERGE_REQUEST_SOURCE_BRANCH_NAME and CI_MERGE_REQUEST_TARGET_BRANCH_NAME must be set")
+
     print("Running git diff --name-only command...")
-    result = subprocess.run(['git', 'diff', '--name-only', 'remotes/origin/main', 'remotes/origin/test', '--', '*.java'], capture_output=True, text=True)
+    result = subprocess.run(['git', 'diff', '--name-only', f'remotes/origin/{target_branch}', f'remotes/origin/{source_branch}', '--', '*.java'], capture_output=True, text=True)
     print("Git diff command output:\n", result.stdout)
-    return result.stdout
 
 def parse_diff_output(diff_output):
     print("Parsing diff output...")
@@ -46,7 +56,7 @@ def count_lines_in_file(file_path):
     except FileNotFoundError:
         print(f"Error: File '{file_path}' not found.")
         return None
-    
+
 def add_sonar_comments(file_changes):
     print(f"Adding Sonar comments to {file_changes}...")
     # Iterate through each file and its changes
@@ -90,19 +100,6 @@ def add_sonar_comments(file_changes):
         print(f"Sonar comments added to {file_path}.")
         print()  # Empty line for readability
 
-def format_output(file_changes):
-    print("Formatting output...")
-    for file, lines in file_changes.items():
-        line_ranges = []
-        for i in range(len(lines)):
-            if i == 0 or lines[i] != lines[i-1] + 1:
-                line_ranges.append([lines[i], lines[i]])
-            else:
-                line_ranges[-1][1] = lines[i]
-
-        formatted_ranges = [f'[{start},{end}]' if start != end else f'[{start}]' for start, end in line_ranges]
-        print(f'{file}: {formatted_ranges}')
-
 def run_gradle_sonar(paths):
     # Convert paths to comma-delimited string
     comma_paths = ','.join(paths)
@@ -127,7 +124,6 @@ def extract_file_paths(diff_output):
 if __name__ == "__main__":
     diff_output = run_git_diff()
     file_changes = parse_diff_output(diff_output)
-    format_output(file_changes)
     add_sonar_comments(file_changes)
     file_names = run_git_diff_for_changed_files_names()
     paths = extract_file_paths(file_names)
